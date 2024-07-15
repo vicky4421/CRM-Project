0abad132bde2c31c216ff9e0206d438ae6e797c4
Vivek Sawant <vicky210490@gmail.com>
2024-07-15 16:00:26 +0530

    define user entity

---
+++ backend/src/main/java/com/vismijatech/main/entity/User.java
@@ -0,0 +1,26 @@
+package com.vismijatech.main.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String phoneNo;
+    private String city;
+}
