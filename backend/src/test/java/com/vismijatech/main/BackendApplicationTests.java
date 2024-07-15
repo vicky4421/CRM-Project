663a437d77f5a3497552c2357f59db3741bba54f
Vivek Sawant <vicky210490@gmail.com>
2024-07-15 14:25:55 +0530

    init

---
+++ backend/src/test/java/com/vismijatech/main/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vismijatech.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
