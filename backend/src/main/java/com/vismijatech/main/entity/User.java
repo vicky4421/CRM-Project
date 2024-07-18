588d6acf45d3013c99f9598863fc1839ba164cc8
Vivek Sawant <vicky210490@gmail.com>
2024-07-18 16:34:20 +0530

    add validation patterns and error messages for every field

--- backend/src/main/java/com/vismijatech/main/entity/User.java
+++ backend/src/main/java/com/vismijatech/main/entity/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,9 +19,19 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Pattern(regexp = "^[a-z,A-Z ]{5,25}$", message = "Invalid name format!")
     private String name;
+
+    @Pattern(regexp = "^[a-zA-Z0-9._%±]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$", message = "Invalid email format!")
     private String email;
+
+    @Pattern(regexp = "^[a-z,A-Z,0-9]{5,25}$", message = "Invalid password format!")
     private String password;
+
+    @Pattern(regexp = "^[0-9]{10}$/", message = "Invalid password format")
     private String phoneNo;
+
+    @Pattern(regexp = "^[a-z,A-Z]{3,25}$", message = "Invalid city format!")
     private String city;
 }
