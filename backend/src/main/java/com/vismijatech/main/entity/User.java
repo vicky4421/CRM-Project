e74804b663cdbb69a8f49908154e5571444f4f79
Vivek Sawant <vicky210490@gmail.com>
2024-07-24 15:24:28 +0530

    change regex for password and phone no.

--- backend/src/main/java/com/vismijatech/main/entity/User.java
+++ backend/src/main/java/com/vismijatech/main/entity/User.java
@@ -26,10 +26,12 @@ public class User {
     @Pattern(regexp = "^[a-zA-Z0-9._%±]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$", message = "Invalid email format!")
     private String email;
 
-    @Pattern(regexp = "^[a-z,A-Z,0-9]{5,25}$", message = "Invalid password format!")
+//    @Pattern(regexp = "^[a-z,A-Z0-9]{5,25}$", message = "Invalid password format!")
+    @Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$", message = "Invalid password format!")
     private String password;
 
-    @Pattern(regexp = "^[0-9]{10}$/", message = "Invalid password format")
+//    @Pattern(regexp = "^[0-9]{10}/", message = "Invalid phone format")
+    @Pattern(regexp = "^[+]?[(]?[0-9]{3}[)]?[-\\s.]?[0-9]{3}[-\\s.]?[0-9]{4,6}$", message = "Invalid phone format")
     private String phoneNo;
 
     @Pattern(regexp = "^[a-z,A-Z]{3,25}$", message = "Invalid city format!")
