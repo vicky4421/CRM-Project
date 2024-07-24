e958f7ca3d62da0a7ea81b1d7c3e34051986d3e1
Vivek Sawant <vicky210490@gmail.com>
2024-07-24 22:04:00 +0530

    define service for Course

---
+++ backend/src/main/java/com/vismijatech/main/service/CourseService.java
@@ -0,0 +1,9 @@
+package com.vismijatech.main.service;
+
+import com.vismijatech.main.entity.Course;
+
+import java.util.List;
+
+public interface CourseService {
+    public List<Course> getAllCourseDetails();
+}
