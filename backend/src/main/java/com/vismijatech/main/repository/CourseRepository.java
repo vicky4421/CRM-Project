ed7e9763e32f0f7f66d8c0f75698a8823ce65e3e
Vivek Sawant <vicky210490@gmail.com>
2024-07-24 22:03:46 +0530

    define repository for Course

---
+++ backend/src/main/java/com/vismijatech/main/repository/CourseRepository.java
@@ -0,0 +1,7 @@
+package com.vismijatech.main.repository;
+
+import com.vismijatech.main.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseRepository extends JpaRepository<Course, Long> {
+}
